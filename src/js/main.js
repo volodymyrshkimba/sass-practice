1b8811a803d42c7bbd32100401ed800c95679e98
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-08-01 23:39:03 +0300

    add base page styles, link fonts, layout styles, and main grid template

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,36 @@
 import "../scss/main.scss";
 
-// import * as bodyScrollLock from "body-scroll-lock";
+import * as bodyScrollLock from "body-scroll-lock";
 
-// // Sidebar menu
-// const refsMenu = {
-//   openMenuBtn: document.querySelector(".js-menu-open"),
-//   closeMenuBtn: document.querySelector(".js-menu-close"),
-//   overlayMenu: document.querySelector(".js-menu"),
-// };
+// Sidebar menu
+const refsMenu = {
+  openMenuBtn: document.querySelector(".js-menu-open"),
+  closeMenuBtn: document.querySelector(".js-menu-close"),
+  overlayMenu: document.querySelector(".js-menu"),
+};
 
-// const toggleMenu = () => {
-//   const isMenuOpen =
-//     refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-//   refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-//   refsMenu.overlayMenu.classList.toggle("is-open");
+const toggleMenu = () => {
+  const isMenuOpen =
+    refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
 
-//   const scrollLockMethod = !isMenuOpen
-//     ? "disableBodyScroll"
-//     : "enableBodyScroll";
-//   bodyScrollLock[scrollLockMethod](document.body);
-// };
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+  refsMenu.overlayMenu.classList.toggle("is-open");
 
-// refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
-// refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
+  const scrollLockMethod = !isMenuOpen
+    ? "disableBodyScroll"
+    : "enableBodyScroll";
+  bodyScrollLock[scrollLockMethod](document.body);
+  bodyScrollLock[scrollLockMethod](document.body);
+};
 
-// // Close the mobile menu on wider screens if the device orientation changes
-// window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
-//   if (!event.matches) return;
+refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
+refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
 
-//   refsMenu.overlayMenu.classList.remove("is-open");
-//   refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
-//   bodyScrollLock.enableBodyScroll(document.body);
-// });
+// Close the mobile menu on wider screens if the device orientation changes
+window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
+  if (!event.matches) return;
+
+  refsMenu.overlayMenu.classList.remove("is-open");
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
